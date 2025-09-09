59e5a0f92817ccbe81f6505f13f01fa9e800e764
MI-Israr <m.israr1228@gamil.com>
2025-09-09 16:23:21 +0500

    GET, PUT, POST, DELETE simple concept

--- making-simple-server/server.js
+++ making-simple-server/server.js
@@ -15,8 +15,28 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+
+const user = {
+  id: 1,
+  name: "Rizwan",
+};
 app.get("/", (req, res) => {
   res.send("New Hello World!");
+});
+app.get("/user", (req, res) => {
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  res.send("this is a post request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("this is a put request");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("this is a delete request");
 });
 
 app.listen(3000, () => {
