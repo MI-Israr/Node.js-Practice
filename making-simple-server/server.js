0176a73aa78fda3912059e2c673381a6f76603eb
MI-Israr <m.israr1228@gmail.com>
2025-09-10 14:41:39 +0500

    Created "/", "/projects", "/blog", "/users" Routes.

--- making-simple-server/server.js
+++ making-simple-server/server.js
@@ -31,17 +31,53 @@
 // });
 
 // Checking github contribution status
+// import http from "http";
+
+// const hostName = "127.0.0.1";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("content-type", "text/plain");
+//   res.end("Hello world! This is a Node.js server");
+// });
+
+// server.listen(port, hostName, () => {
+//   console.log(`Server is running on port: ${port}, on Host ${hostName}`);
+// });
+
 import http from "http";
 
-const hostName = "127.0.0.1";
-const port = 3000;
+let users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("content-type", "text/plain");
-  res.end("Hello world! This is a Node.js server");
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is the Home Page");
+  }
+  if (req.method === "GET" && req.url === "/projects") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is a Projects page");
+  }
+  if (req.method === "GET" && req.url === "/blog") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is the Blog Page");
+  }
+
+  if (req.method === "GET" && req.url === "/users") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "This is the Users Page",
+        data: users,
+      })
+    );
+  }
 });
 
-server.listen(port, hostName, () => {
-  console.log(`Server is running on port: ${port}, on Host ${hostName}`);
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server is running on port 3000, host name is 127.0.0.1");
 });
