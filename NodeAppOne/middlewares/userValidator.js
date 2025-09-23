55a53124df671e02ecd9af3b8e97de140a9c76dc
MI-Israr <m.israr1228@gmail.com>
2025-09-23 12:30:44 +0500

    validate error fixed

--- NodeAppOne/middlewares/userValidator.js
+++ NodeAppOne/middlewares/userValidator.js
@@ -2,8 +2,7 @@ export const validate = (schema) => (req, res, next) => {
   const result = schema.safeParse(req.body);
 
   if (!result.success) {
-    const errors = result.error.errors.map((err) => err.message);
-    return res.status(400).json({ errors });
+    return res.status(400).json({ error: "feilds not allowed" });
   }
 
   next();
